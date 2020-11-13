8a4adc57dca5ee7408a4b892d1ed6447c9797b09
Richard Astbury <richard.astbury@gmail.com>
2020-11-13 15:41:47 +0000

    add new norway, remove broken locations

--- index.jsx
+++ index.jsx
@@ -147,8 +147,7 @@ const Table = class extends React.Component {
           <ul>
             <li><a href="https://github.com/ncareau">NMC</a> for the storage accounts in Canada.</li>
             <li><a href="https://github.com/jurajsucik">Juraj Sucik</a> for the storage accounts in Switzerland and Germany North.</li>
-            <li><a href="https://github.com/mrnof">mrnof</a> for the storage accounts in Australia Central, Norway and Brazil.</li>
-          </ul>
+1          </ul>
         </p>
         <p>
           The{' '}
