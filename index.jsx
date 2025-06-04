c39f2f420da9a13e910fb0d6be776730c8f6f54b
Tim אבי סוורד Nilimaa-Svärd <TimNilimaa@users.noreply.github.com>
2025-06-04 05:38:08 +0000

    Removed credit to Brukarkooperativet JAG
    This is since Brukarkooperativet JAG is no longer hosting the Sweden Central location.

--- index.jsx
+++ index.jsx
@@ -148,7 +148,6 @@ const Table = class extends React.Component {
           <ul>
             <li><a href="https://github.com/ncareau">NMC</a> for the storage accounts in Canada.</li>
             <li><a href="https://github.com/jurajsucik">Juraj Sucik</a> for the storage accounts in Switzerland and Germany North.</li>
-            <li><a href="https://github.com/brukarkooperativet-jag">Brukarkooperativet JAG</a> for the storage account in Sweden</li>
             <li><a href="https://github.com/wi5nia">Tomasz Wisniewski</a> for the storage account in Poland Central</li>
           </ul>
         </p>
